eff5e9b01632fe4d20e54c46adcd711586dc0fb8
muqeet007 <muqeetnabila308@gmail.com>
2025-08-22 15:15:49 +0500

    Database connection completed.

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,28 @@
+import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config()
 
 
 export const PORT=process.env.PORT
+
+// MongoDB connection
+
+export const connectDataBase=async()=>
+{
+    try
+    {
+        await mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        })
+
+        console.log("Database Connection : SUCCESS");
+     }
+
+     catch
+     {
+        console.log("Database Connection : FAILED")
+        process.exit(1)
+     }  
+}
+
--- server/server.js
+++ server/server.js
@@ -1,9 +1,29 @@
+import { connectDataBase } from './config/config.js';
 import app from './app.js'
 import {PORT} from './config/config.js'
 
 
-app.listen(PORT,()=>
+const startServer=async()=>
+{
+    
+    try
+    {
+        await connectDataBase()
+
+    app.listen(PORT,()=>
     {
         console.log(`The server is running on ${PORT}`);
         
     })
+    }
+
+    catch(error)
+    {
+        console.log(`Failed to connect to the server: ${error.message}`);
+        
+    }
+}
+
+startServer()
+
+
